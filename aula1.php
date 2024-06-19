8c7ac72e7f52f8e3e5e62e2dbd69250d65898fb1
StephanySilvaRibeiro <stephany_silva2000@outlook.com>
2024-06-18 21:57:34 -0300

    Primeiro Commit no GitHub

---
+++ aula1.php
@@ -0,0 +1,33 @@
+<?php
+//Esse é um comentário do código, não será exibido em tela.
+# Outro tipo de comentário.
+/* Mais um tipo de comentário. */
+
+// <br> = quebra de linha.
+
+echo "Olá mundo <br>";
+echo "<br>";
+
+$texto = "Pequeno texto que você está lendo sobre o livro do Menino Maluquinho!";
+echo $texto;
+
+echo "<br>"; // Pular linha.
+
+$outraVariavel = "Outro grande texto!"; // Aqui é um comentário.
+echo $outraVariavel;
+
+echo "<br> Calculos Matemáticos<br>";
+echo 1+1;
+echo "<br>";
+echo 10-7;
+echo "<br>";
+echo 99*180;
+echo "<br>";
+echo 50/7;
+echo "<br>";
+echo 4%2;
+echo "<br>";
+echo 5%2;
+
+
+?>
