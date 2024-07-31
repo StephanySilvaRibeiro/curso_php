abe40b246463c396678cd2ee43256bc32241857b
StephanySilvaRibeiro <stephany_silva2000@outlook.com>
2024-07-30 21:58:04 -0300

    Fazer tarefa de casa

---
+++ aula7.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+$vetor = array(); // Vetor vazio
+//$vetor = [5, 2, 3, 4, 1];
+// $i =     0, 1, 2, 3, 4
+
+
+$tamanho = count($vetor);
+echo $vetor[5] . "<br>"; 
+
+for($i = 0; $i < $tamanho; $i++){
+    echo $vetor[$i] . "<br>";
+
+    if ($i % 2 == 0) {
+
+        $somaPares += $i;
+
+    } else {
+
+        $somaImpares = $somaImpares + $i;
+
+    }
+}
+
+*/
+
+// --------------------------------------------------------------------------------------------------
+
+
+$vetor = array();
+$vetorPar = [];
+$vetorImpar = [];
+
+
+for($i = 1; $i <= 100; $i++){
+    
+    if ($i % 2 == 0) {
+
+        $somaPares += $i;
+        // $vetorPar = array_push($vetorPar, $i);
+        $vetorPar[] = $i;
+
+    } else {
+
+        $somaImpares += $i;
+        // $vetorImpar = array_push($vetorImpar, $i);
+        $vetorImpar[] = $i;
+    }
+}
+
+
+$tamanhoPar = count($vetorPar);
+$tamanhoImpar = count($vetorImpar);
+
+
+// Colocar os números em ordem decrescente
+// Usar variaveis
+
+for($i = 0; $i < $tamanhoPar; $i++){
+
+
+}
+for($i = 0; $i < $tamanhoImpar; $i++){
+
+
+}
+
+
+
+
+echo "NÚMEROS PARES: <br>";
+
+// for($i = 0; $i < count($vetorPar); $i++)
+for($i = 0; $i < $tamanhoPar; $i++){
+
+    echo $vetorPar[$i] . "- ";
+
+}
+
+echo "<br>";
+echo "<br>";
+echo "NÚMEROS IMPARES: <br>";
+
+// for($i = 0; $i < count($vetorImpar); $i++)
+for($i = 0; $i < $tamanhoImpar; $i++){
+
+    echo $vetorImpar[$i] . "- ";
+}
