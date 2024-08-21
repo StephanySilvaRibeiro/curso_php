5212c2e4604c4c7d2ccc392fadbd34dcfdfae770
StephanySilvaRibeiro <stephany_silva2000@outlook.com>
2024-08-20 21:57:24 -0300

    LicaoDeCasa

---
+++ aula10.php
@@ -0,0 +1,7 @@
+<?php
+
+// Criar duas funções separadas onde cada uma recebe um parametro (string, texto) e retorna um valor boolean (true || false, 0 || 1)
+// Objetivo: Validar um CPF, CNPJ
+
+// Sugestão / dica paara o nome da função: isValidCPF, isValidCNPJ
+// Calcular CPF valido. Pesquisar
--- aula9.php
+++ aula9.php
@@ -72,9 +72,10 @@ echo "SISTEMA BANCÁRIO";
 echo "<br>";
 echo "<br>";
 
-function Banco($valor, $operacao){
+/*
 
-    $saldoAtual = 200;
+function Banco($valor, $operacao, $saldoAtual){
+
 
     if ($operacao == "Deposito"){
 
@@ -97,11 +98,81 @@ function Banco($valor, $operacao){
     }
 }
 
-$valor = 2000;
-$operacao = "Extrato";
+$saldoAtual=2000; // Saldo inicial
+echo "SALDO INICIAL: $saldoAtual <br>";
 
+$valor = 3000;
+$operacao = "Deposito";
 
-$saldoAtual = Banco($valor, $operacao);
+$saldoAtual = Banco($valor, $operacao, $saldoAtual);
 
 echo "SEU SALDO ATUAL É: $saldoAtual <br> OPERAÇÃO REALIZADA: $operacao <br>";
+
+
+echo "<br>";
+
+
+$valor = 3000;
+$operacao = "Saque";
+
+$saldoAtual = Banco($valor, $operacao, $saldoAtual);
+
+echo "SEU SALDO ATUAL É: $saldoAtual <br> OPERAÇÃO REALIZADA: $operacao <br>";
+
+*/
+
+//-----------------------------------------------------------------------------------------
+
+// Não permitir DEPOSITO em branco ou 0.
+// Não permitir que o SAQUE seja maior do que tem na conta.
+
+
+function banco($valor, $operacao, $saldoAtual) {
+
+    $saldo = $saldoAtual;
+
+    if ($operacao == "Extrato") {
+        return $saldo;
+    }
+
+    if ($operacao == "Saque" && $valor > $saldoAtual) {
+
+        return "Você não tem dinheiro suficiente.";
+    }
+
+    if ($operacao == "Saque" && ($valor <=0 || $valor == "")) {
+
+        return "O deposito esta em branco, verifique o valor informado.";
+    }
+
+    if ($operacao == "Saque") {
+        // $saldo = $saldo - $valor;
+        $saldo -= $valor;
+    }
+
+    if ($operacao == "Deposito" && ($valor <=0 || $valor == "")) {
+
+        return "O deposito esta em branco, verifique o valor informado.";
+    }
+
+    if ($operacao == "Deposito") {
+        // $saldo = $saldo + $valor;
+        $saldo += $valor;
+    }
+    
+    return $saldo;
+}
+
+$saldoAtual = 500; // Tem um valor inicial.
+echo "VALOR INICIAL: $saldoAtual <br><br>";
+
+$valor = 100;
+$operacao = "Deposito";
+$saldoAtual = banco($valor, $operacao, $saldoAtual);
+echo "$operacao = $valor <br> EXTRATO: $saldoAtual<br><br>";
+
+$valor = 700;
+$operacao = "Saque";
+$saldoAtual = banco($valor, $operacao, $saldoAtual);
+echo "$operacao = $valor <br> EXTRATO: $saldoAtual<br><br>";
 
