1d1822cff478ba6316ecc8c11375376a3285b556
StephanySilvaRibeiro <stephany_silva2000@outlook.com>
2024-09-24 21:34:53 -0300

    aula14.php

--- aula14.php
+++ aula14.php
@@ -31,6 +31,28 @@ class Usuario{
 
 }
 
+// NO ID É PRECISO IR EM RESTRIÇÕES E DEIXAR ELA COMO PRIMARY KEY
+
+// Criar mais 4 campos na tabela de usuario:
+// excluido (bolean), data_criacao (datetime), data_atualizacao (datetime), usuario_alteracao(varchar 50)
+
+
+
+// CRIAR TABELA PESSOA:
+// nome --> varchar(500)
+// sobrenome --> varchar(500)
+// nome_completo --> varchar(1000)
+// sexo --> enum
+// email --> varchar(255)
+// cpf --> varchar(11)
+// cnpj --> varchar(14)
+// tipo_pessoa (J ou F) --> enum
+// estado_civil --> enum
+// data_nascimento -- datetime
+// id_usuario --> smallint
+
+
+
 
 
 
